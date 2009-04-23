91db8559c05adcaff89e18641bbe382a006e55b9
MrSuborbian88 <MrSuborbian88@2066e828-f250-11dd-87c9-111d11f6bf44>
2009-04-23 16:55:00 +0000

    Removed small bug in reports.
    Reports was still linking to IP Lookup which we removed so I removed the column in the reports/live logs which referenced it.

--- moodle/course/lib.php
+++ moodle/course/lib.php
@@ -295,10 +295,10 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 		echo "<th class=\"c0 header\" scope=\"col\">".get_string('course')."</th>\n";
 	}
 	echo "<th class=\"c1 header\" scope=\"col\">".get_string('time')."</th>\n";
-	echo "<th class=\"c2 header\" scope=\"col\">".get_string('ip_address')."</th>\n";
-	echo "<th class=\"c3 header\" scope=\"col\">".get_string('fullname')."</th>\n";
-	echo "<th class=\"c4 header\" scope=\"col\">".get_string('action')."</th>\n";
-	echo "<th class=\"c5 header\" scope=\"col\">".get_string('info')."</th>\n";
+//	echo "<th class=\"c2 header\" scope=\"col\">".get_string('ip_address')."</th>\n";
+	echo "<th class=\"c2 header\" scope=\"col\">".get_string('fullname')."</th>\n";
+	echo "<th class=\"c3 header\" scope=\"col\">".get_string('action')."</th>\n";
+	echo "<th class=\"c4 header\" scope=\"col\">".get_string('info')."</th>\n";
 	echo "</tr>\n";
 
 	// Make sure that the logs array is an array, even it is empty, to avoid warnings from the foreach.
@@ -346,14 +346,14 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 		}
 		echo "<td class=\"cell c1\" align=\"right\">".userdate($log->time, '%a').
 			 ' '.userdate($log->time, $strftimedatetime)."</td>\n";
-		echo "<td class=\"cell c2\">\n";
-		link_to_popup_window("/iplookup/index.php?ip=$log->ip&amp;user=$log->userid", 'iplookup',$log->ip, 440, 700);
-		echo "</td>\n";
+//		echo "<td class=\"cell c2\">\n";
+//		link_to_popup_window("/iplookup/index.php?ip=$log->ip&amp;user=$log->userid", 'iplookup',$log->ip, 440, 700);
+//		echo "</td>\n";
 		$fullname = fullname($log, has_capability('moodle/site:viewfullnames', get_context_instance(CONTEXT_COURSE, $course->id)));
-		echo "<td class=\"cell c3\">\n";
+		echo "<td class=\"cell c2\">\n";
 		echo "	<a href=\"$CFG->wwwroot/user/view.php?id={$log->userid}&amp;course={$log->course}\">$fullname</a>\n";
 		echo "</td>\n";
-		echo "<td class=\"cell c4\">\n";
+		echo "<td class=\"cell c3\">\n";
 		$displayaction="$log->module $log->action";
 		if($brokenurl) {
 			echo $displayaction;
@@ -361,7 +361,7 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 			link_to_popup_window( make_log_url($log->module,$log->url), 'fromloglive',$displayaction, 440, 700);
 		}
 		echo "</td>\n";;
-		echo "<td class=\"cell c5\">{$log->info}</td>\n";
+		echo "<td class=\"cell c4\">{$log->info}</td>\n";
 		echo "</tr>\n";
 	}
 	echo "</table>\n";
@@ -411,10 +411,10 @@ function print_mnet_log($hostid, $course, $user=0, $date=0, $order="l.time ASC",
 		echo "<th class=\"c0 header\">".get_string('course')."</th>\n";
 	}
 	echo "<th class=\"c1 header\">".get_string('time')."</th>\n";
-	echo "<th class=\"c2 header\">".get_string('ip_address')."</th>\n";
-	echo "<th class=\"c3 header\">".get_string('fullname')."</th>\n";
-	echo "<th class=\"c4 header\">".get_string('action')."</th>\n";
-	echo "<th class=\"c5 header\">".get_string('info')."</th>\n";
+//	echo "<th class=\"c2 header\">".get_string('ip_address')."</th>\n";
+	echo "<th class=\"c2 header\">".get_string('fullname')."</th>\n";
+	echo "<th class=\"c3 header\">".get_string('action')."</th>\n";
+	echo "<th class=\"c4 header\">".get_string('info')."</th>\n";
 	echo "</tr>\n";
 
 	if (empty($logs['logs'])) {
@@ -454,17 +454,17 @@ function print_mnet_log($hostid, $course, $user=0, $date=0, $order="l.time ASC",
 		}
 		echo "<td class=\"r$row c1\" align=\"right\">".userdate($log->time, '%a').
 			 ' '.userdate($log->time, $strftimedatetime)."</td>\n";
-		echo "<td class=\"r$row c2\" >\n";
-		link_to_popup_window("/iplookup/index.php?ip=$log->ip&amp;user=$log->userid", 'iplookup',$log->ip, 400, 700);
-		echo "</td>\n";
+//		echo "<td class=\"r$row c2\" >\n";
+//		link_to_popup_window("/iplookup/index.php?ip=$log->ip&amp;user=$log->userid", 'iplookup',$log->ip, 400, 700);
+//		echo "</td>\n";
 		$fullname = fullname($log, has_capability('moodle/site:viewfullnames', get_context_instance(CONTEXT_COURSE, $course->id)));
-		echo "<td class=\"r$row c3\" >\n";
+		echo "<td class=\"r$row c2\" >\n";
 		echo "	<a href=\"$CFG->wwwroot/user/view.php?id={$log->userid}\">$fullname</a>\n";
 		echo "</td>\n";
-		echo "<td class=\"r$row c4\">\n";
+		echo "<td class=\"r$row c3\">\n";
 		echo $log->action .': '.$log->module;
 		echo "</td>\n";;
-		echo "<td class=\"r$row c5\">{$log->info}</td>\n";
+		echo "<td class=\"r$row c4\">{$log->info}</td>\n";
 		echo "</tr>\n";
 	}
 	echo "</table>\n";
